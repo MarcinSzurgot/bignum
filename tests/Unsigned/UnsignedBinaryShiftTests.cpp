d6ef71aebc569cc74b46919264d3eeb2fe44584b
Zbigniew Kruszyński <ramcin908@wp.pl>
2019-12-02 01:01:29 +0100

    improved left bit shift operator unit test

--- tests/Unsigned/UnsignedBinaryShiftTests.cpp
+++ tests/Unsigned/UnsignedBinaryShiftTests.cpp
@@ -6,16 +6,27 @@ TEST(UnsignedBinaryShiftTests, testThatLeftShiftsWholeDigits)
 {
     // given
     const auto wholeDigitOffset = sizeof(bignum::digit_type) * 8u;
-    const auto digit = bignum::digit_type(123);
-    const auto offsets = 100;
+    const auto expected = bignum::Unsigned({123, 53, 97});
+    const auto digitOffsets = 10;
 
-    for(auto offset = 0u; offset < offsets; ++offset)
+    for(auto digitOffset = 0u; digitOffset < digitOffsets; ++digitOffset)
     {
         // when
-        const auto given = bignum::Unsigned(digit) << offset * wholeDigitOffset;
+        const auto given = expected << digitOffset * wholeDigitOffset;
 
         // then
-        ASSERT_EQ(given.magnitude(), offset + 1);
-        ASSERT_EQ(given.digit(given.magnitude() - 1), digit);
+        ASSERT_EQ(given.magnitude(), expected.magnitude() + digitOffset);
+
+        // offsetted part should be the same
+        for(auto digit = 0u; digit < expected.magnitude(); ++digit)
+        {
+            ASSERT_EQ(given.digit(digit + digitOffset), expected.digit(digit));
+        }
+
+        // extra digits should be zero
+        for(auto digit = 0u; digit < digitOffset; ++digit)
+        {
+            ASSERT_EQ(given.digit(digit), bignum::digit_type());
+        }
     }
 }
