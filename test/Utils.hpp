4446a5fe93bcf40a56e8b83480631bc4e074a85a
Marcin Szurgot <ramcin908@gmail.com>
2023-11-26 16:34:17 +0100

    moved tests for addition and subtraction to array tests

--- test/Utils.hpp
+++ test/Utils.hpp
@@ -1,6 +1,9 @@
 #include <bignum/BigUnsigned.hpp>
 
+#include <concepts>
 #include <iostream>
+#include <span>
+#include <vector>
 
 // inline std::ostream& operator<<(std::ostream& os, const BigUnsigned& num) {
 //     return os << static_cast<std::string>(num);
@@ -13,3 +16,27 @@ inline std::ostream& operator<<(std::ostream& os, const BigUnsigned& num) {
     }
     return os << "}";
 }
+
+template<std::unsigned_integral U>
+inline std::ostream& operator<<(std::ostream& os, const std::vector<U>& num) {
+    os << std::hex << "{0x" << num[0];
+    for (auto d = 1u; d < size(num); ++d) {
+        os << ", 0x" << num[d];
+    }
+    return os << "}";
+}
+
+template<std::unsigned_integral U>
+inline std::ostream& operator<<(std::ostream& os, std::span<const U> num) {
+    os << std::hex << "{0x" << num[0];
+    for (auto d = 1u; d < size(num); ++d) {
+        os << ", 0x" << num[d];
+    }
+    return os << "}";
+}
+
+struct BinaryOpParams {
+    std::vector<std::uint32_t> lhs;
+    std::vector<std::uint32_t> rhs;
+    std::vector<std::uint32_t> expected;
+};
